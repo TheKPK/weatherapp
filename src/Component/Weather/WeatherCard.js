a96efcb575028192614545f50a8c07ce3f09d965
Pratibha Kumar Kashapogu <pratibha.kk@incedoinc.com>
2022-01-14 13:31:28 +0530

    fixed the code from http to https

--- src/Component/Weather/WeatherCard.js
+++ src/Component/Weather/WeatherCard.js
@@ -1,111 +1,121 @@
-import React, {useEffect} from 'react';
-import './style.css'
+import React, { useEffect } from "react";
+import "./style.css";
 
-const WeatherCard = ({tempInfo}) => {
-	const [weatherState, setWeatheState] = React.useState("");
-	const {temp, humidity, pressure, weatherMood, speed, country,name} = tempInfo;
+const WeatherCard = ({ tempInfo }) => {
+  const [weatherState, setWeatheState] = React.useState("");
+  const {
+    temp,
+    humidity,
+    pressure,
+    weatherMood,
+    speed,
+    country,
+    sunset,
+    sunrise,
+    name,
+    timezone,
+  } = tempInfo;
 
-	//converting seconds into time
-	let date = new Date();
-	let timeStr = `${date.getHours()}:${date.getMinutes()}`
-	let ampm = (timeStr >= `12:00`) ? "PM" : "AM"
+  //converting seconds int time
 
+  let sec = sunset;
+  let date = new Date(sec * 1000);
+  let timeStr = `${date.getHours()}:${date.getMinutes()}`;
+  let ampm = timeStr >= `12:00` ? "PM" : "AM";
 
-	useEffect(() => {
-		if (weatherMood) {
-			switch (weatherMood) {
-				case "Clouds":
-					setWeatheState("wi-day-cloudy");
-					break;
-				case "Haze":
-					setWeatheState("wi-fog");
-					break;
-				case "Clear":
-					setWeatheState("wi day-sunny");
-					break;
-				case "Mist":
-					setWeatheState("wi-dust");
-					break;
+  useEffect(() => {
+    if (weatherMood) {
+      switch (weatherMood) {
+        case "Clouds":
+          setWeatheState("wi-day-cloudy");
+          break;
+        case "Haze":
+          setWeatheState("wi-fog");
+          break;
+        case "Clear":
+          setWeatheState("wi day-sunny");
+          break;
+        case "Mist":
+          setWeatheState("wi-dust");
+          break;
 
-				default:
-					setWeatheState("wi-day-sunny");
-					break;
-			}
-		}
-	}, [weatherMood])
-	return (
-		<>
+        default:
+          setWeatheState("wi-day-sunny");
+          break;
+      }
+    }
+  }, []);
+  return (
+    <>
+      <article className="widget">
+        <div className="weatherIcon">
+          <i className={`wi ${weatherState}`} />
+        </div>
 
-			<article className="widget">
-				<div className="weatherIcon">
-					<i className={`wi ${weatherState}`}/>
-				</div>
+        <div className="weatherInfo">
+          <div className="temperature">
+            <span>{temp}&deg;</span>
+          </div>
 
-				<div className="weatherInfo">
-					<div className="temperature">
-						<span>{temp}&deg;</span>
-					</div>
+          <div className="description">
+            <div className="weatherCondition">{weatherMood}</div>
+            <div className="place">
+              {name},{country}
+            </div>
+          </div>
+        </div>
 
-					<div className="description">
-						<div className="weatherCondition">{weatherMood}</div>
-						<div className="place">
-							{name},{country}
-						</div>
-					</div>
-				</div>
+        <div className="date"> {new Date().toLocaleString()} </div>
 
-				<div className="date"> {new Date().toLocaleString()} </div>
+        {/* our 4column section  */}
+        <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {timeStr} {ampm} <br />
+                Sunset
+              </p>
+            </div>
 
-				{/* our 4column section  */}
-				<div className="extra-temp">
-					<div className="temp-info-minmax">
-						<div className="two-sided-section">
-							<p>
-								<i className={"wi wi-sunset"}></i>
-							</p>
-							<p className="extra-info-leftside">
-								{timeStr}:{ampm} <br/>
-								Sunset
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-humidity"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {humidity} <br />
+                Humidity
+              </p>
+            </div>
+          </div>
 
-							</p>
-						</div>
+          <div className="weather-extra-info">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-rain"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {pressure} <br />
+                Pressure
+              </p>
+            </div>
 
-						<div className="two-sided-section">
-							<p>
-								<i className={"wi wi-humidity"}></i>
-							</p>
-							<p className="extra-info-leftside">
-								{humidity} <br/>
-								Humidity
-							</p>
-						</div>
-					</div>
-
-					<div className="weather-extra-info">
-						<div className="two-sided-section">
-							<p>
-								<i className={"wi wi-rain"}></i>
-							</p>
-							<p className="extra-info-leftside">
-								{pressure} <br/>
-								Pressure
-							</p>
-						</div>
-
-						<div className="two-sided-section">
-							<p>
-								<i className={"wi wi-strong-wind"}></i>
-							</p>
-							<p className="extra-info-leftside">
-								{speed} <br/>
-								Speed
-							</p>
-						</div>
-					</div>
-				</div>
-			</article>
-		</>
-	);
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-strong-wind"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {speed} <br />
+                Speed
+              </p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </>
+  );
 };
 
 export default WeatherCard;
--- src/Component/Weather/temp.js
+++ src/Component/Weather/temp.js
@@ -1,55 +1,76 @@
-import React, {useEffect, useState} from 'react';
-import './style.css'
+import React, { useEffect, useState } from "react";
+import "./style.css";
 import WeatherCard from "./WeatherCard";
 
 const Temp = () => {
-	const [searchValue, setSearchValue] = useState('hyderabad');
-	const [tempInfo, setTempInfo] = useState({})
+  const [searchValue, setSearchValue] = useState("hyderabad");
+  const [tempInfo, setTempInfo] = useState({});
 
-	const getWeatherInfo = async () => {
-		try {
-			let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
-			const response = await fetch(url);
-			const data = await response.json();
-			// console.log(data)
-			const {name} = data;
-			const {temp, humidity, pressure} = data.main;
-			const {main: weatherMood} = data.weather[0];
-			const {speed, deg} = data.wind;
-			const {country, sunset} = data.sys;
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
+      const response = await fetch(url);
+      const data = await response.json();
 
-			//    creating a object based on above values
-			const myNewObject = {
-				temp, humidity, pressure, weatherMood, speed, country, sunset, name, deg
-			}
-			// console.log(myNewObject)
-			setTempInfo(myNewObject)
+      const { name,timezone} = data;
+      const { temp, humidity, pressure } = data.main;
+      const { main: weatherMood } = data.weather[0];
+      const { speed, deg } = data.wind;
+      const { country, sunset,sunrise } = data.sys;
 
-		} catch (error) {
-			console.log({
-				error: "404",
-				message: "city not found"
-			})
+      //    creating a object based on above values
+      const myNewObject = {
+        temp,
+        humidity,
+        pressure,
+        weatherMood,
+        speed,
+        country,
+        sunset,
+        sunrise,
+        name,
+        deg,
+        timezone
+      };
 
-		}
-	}
-	useEffect(() => {
-		getWeatherInfo()
-	}, [])
+      setTempInfo(myNewObject);
+    } catch (error) {
+      console.log({
+        error: "404",
+        message: "city not found",
+      });
+    }
+  };
 
-	return (
-		<>
-			<div className="wrap">
-				<div className="search">
-					<input type="search" placeholder="Search...." autoFocus id="search" className="searchTerm"
-								 value={searchValue} onChange={(e) => setSearchValue(e.target.value)}/>
-					<button className="searchButton" type="button" onClick={getWeatherInfo}>Search</button>
-				</div>
-			</div>
-			<WeatherCard tempInfo={tempInfo}/>
-		</>
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
 
-	);
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="Search...."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <WeatherCard tempInfo={tempInfo} />
+    </>
+  );
 };
 
 export default Temp;
